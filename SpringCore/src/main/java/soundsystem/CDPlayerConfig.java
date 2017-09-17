7fb474ffd368ab0e654e956db375e4c1646be0d5
dn <dong.ning@qq.com>
2017-09-17 17:07:18 +0800

    2.2.3 v1

--- SpringCore/src/main/java/soundsystem/CDPlayerConfig.java
+++ SpringCore/src/main/java/soundsystem/CDPlayerConfig.java
@@ -4,7 +4,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+//设置扫描的基础包
+@ComponentScan("soundsystem")
+//@ComponentScan(basePackages = "soundsystem")
+//多个基础包
+//@ComponentScan(basePackages = {"soundsystem","video"})
+//类名设置基础包
+//@ComponentScan(basePackageClasses = {CDPlayer.class,DVDPlayer.class})
 public class CDPlayerConfig {
 
 }
