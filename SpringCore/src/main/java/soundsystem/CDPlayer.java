8e9c3d92637cbebe6beaf38641b0cf946504c0f0
dn <dong.ning@qq.com>
2017-10-08 17:57:17 +0800

    2.4.4 v1

--- SpringCore/src/main/java/soundsystem/CDPlayer.java
+++ SpringCore/src/main/java/soundsystem/CDPlayer.java
@@ -5,25 +5,29 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CDPlayer implements MediaPlayer {
-    private CompactDisc cd;
+    //private CompactDisc cd;
 
+    private CompactDisc compactDisc;
+
+    /*
     @Autowired(required = false)
     public CDPlayer(CompactDisc cd){
         this.cd = cd;
     }
+    */
 
-    /*
     @Autowired
-    public void setCompactDisk(CompactDisc cd){
-        this.cd = cd;
+    public void setCompactDisc(CompactDisc compactDisc){
+        this.compactDisc = compactDisc;
     }
 
+    /*
     @Autowired
     public void insertDisc(CompactDisc cd){
         this.cd = cd;
     }
     */
     public void play() {
-        cd.play();
+        compactDisc.play();
     }
 }
